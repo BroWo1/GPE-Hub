cd70791bd13edbb7ac482ba3097e43b4cd2d5a15
BroWo1 <zhangyang.li@icloud.com>
2025-01-30 00:54:49 +0800

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,95 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Tray, Menu} = require('electron')
+const path = require('node:path')
+
+let tray = null
+let mainWindow = null
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: "static/imgs/logo.ico",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('close', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault()
+      mainWindow.hide()
+    }
+    return false
+  })
+}
+
+function createTray() {
+  // Initialize tray with icon
+  tray = new Tray("static/imgs/logo.ico") // Update the path as necessary
+
+  // Define a context menu for the tray icon
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open',
+      click: () => {
+        mainWindow.show()
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuitting = true
+        app.quit()
+      }
+    }
+  ])
+
+  tray.setToolTip('GPE Hub') // Tooltip when hovering over the tray icon
+  tray.setContextMenu(contextMenu)
+
+  // Optionally, toggle main window visibility on tray icon click
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.show()
+    }
+  })
+}
+
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  createTray()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+app.on('before-quit', () => {
+  app.isQuitting = true
+})
